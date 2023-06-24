0d079c33c311b141c44068f198d55ad353487fca
KanishVlad <130839651+KanishVlad@users.noreply.github.com>
2023-06-24 20:59:10 +0300

    added section popular

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,28 @@
 $(function(){
  
+  var mixer = mixitup('.card-burger', {
+    "animation": {
+      "duration": 700,
+      "nudge": false,
+
+    }
+  });
+
+  var mixer = mixitup('.card-pizza', {
+    "animation": {
+      "duration": 700,
+      "nudge": false,
+
+    }
+  });
+
+  $('.card-pizza').hide();
+
+  
+  $('.popular__btn').one('click', function () {
+    $('.card-pizza').show();
+  });
+
+
+
 })
